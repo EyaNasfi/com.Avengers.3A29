bad24e0e8f186e013d17157cbd57f88a4cebadab
khouloudabid09 <101275597+khouloudabid09@users.noreply.github.com>
2024-02-28 08:12:57 +0100

    user

--- src/main/java/tn/esprit/crud/test/HelloApplication.java
+++ src/main/java/tn/esprit/crud/test/HelloApplication.java
@@ -11,6 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/tn/esprit/crud/AfficherUsers.fxml"));
+       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/tn/esprit/crud/login.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
